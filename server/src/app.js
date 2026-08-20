eff0869df31d0e6321d6df2240bea0588fd69c5d
Rayipudi Pavan <pavanrayipudi1159@gmail.com>
2026-08-20 22:16:15 +0530

    removed client url

--- server/src/app.js
+++ server/src/app.js
@@ -16,12 +16,7 @@ const errorMiddleware = require("./middleware/errorMiddleware");
 
 const app = express();
 
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL,
-        credentials: true,
-    })
-);
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
